eb187c62a0c36c11706d6f592a4602523790f22c
Aren Sinanian <95931889+arensin8@users.noreply.github.com>
2024-09-10 13:20:13 +0000

    Some addings

--- TaskManager/config/db.js
+++ TaskManager/config/db.js
@@ -2,11 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected");
+    await mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
   } catch (err) {
     console.error(`Error: ${err.message}`);
     process.exit(1);
